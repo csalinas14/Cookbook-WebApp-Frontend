81a2cf01e932c5ab903dfd175a9e69f2fa8c3f7d
Carlos Salinas <salinas.carlos14@gmail.com>
2023-09-19 14:39:14 -0400

    First frontend commit

---
+++ README.md
@@ -0,0 +1,1 @@
+CookBook Web Application Frontend
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+//import { useState } from 'react'
+
+const App = () => {
+  return (
+    <div className='mx-auto flex items-center'>
+      <div>
+        <p className='text-4xl'>Search</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
