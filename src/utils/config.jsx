a581f090bc3c95d5e7b2ee7cc7b3e99cd6bb7bfb
Carlos Salinas <salinas.carlos14@gmail.com>
2023-10-02 22:02:56 -0400

    Updating config

--- src/utils/config.jsx
+++ src/utils/config.jsx
@@ -1,8 +1,5 @@
 import 'dotenv/config'
 
-const API_URL =
-  process.env.NODE_ENV === 'production'
-    ? process.env.API_URL
-    : 'http://localhost:3003'
+const API_URL = process.env.API_URL || 'http://localhost:3003'
 
 export default { API_URL }
